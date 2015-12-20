555a7dd5111cdda310cc1cabab612a147ee0700f
Damnever <dxc.wolf@gmail.com>
2015-12-20 12:52:24 +0800

    ...

--- erlou/utils.py
+++ erlou/utils.py
@@ -9,15 +9,15 @@ import pkgutil
 
 root_pkg = os.path.basename(os.path.dirname(os.path.abspath(__file__)))
 
-def load_module_attrs(pkg_path, func, recursive=False, attrs=None):
-    if attrs is None:
-        attrs = list()
+def load_module_attrs(pkg_path, func, recursive=False):
+    attrs = list()
     pkg_name = root_pkg + pkg_path.split(root_pkg)[1].replace('/', '.')
 
     for imp, name, ispkg in pkgutil.iter_modules([pkg_path]):
         if ispkg and recursive:
             next_path = os.path.join(pkg_path, name)
-            return load_module_attrs(next_path, func, recursive, attrs)
+            attrs.extend(load_module_attrs(next_path, func, recursive))
+            continue
 
         module = importlib.import_module('.' + name, pkg_name)
         attr = func(module)
