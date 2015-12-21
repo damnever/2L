875a18e662f36875e477853673e9563b5112e4be
Damnever <dxc.wolf@gmail.com>
2015-12-21 20:15:08 +0800

    :broken_heart:

--- app/service/api/users.py
+++ app/service/api/users.py
@@ -3,7 +3,7 @@
 from __future__ import print_function, division, absolute_import
 
 from app.base.handlers import APIHandler
-from app.base.decorators import authenticated, as_json
+from app.base.decorators import as_json
 
 
 class UsersAPIHandler(APIHandler):
@@ -13,50 +13,54 @@ class UsersAPIHandler(APIHandler):
         return None
 
 
-class AuthedUserAPIHandler(APIHandler):
+class UserAPIHandler(APIHandler):
 
-    @authenticated
     @as_json
     def get(self):
         return None
 
-    @authenticated
     @as_json
     def patch(self):
         return None
 
 
-class AuthedFollowingAPIHandler(APIHandler):
+class FollowingAPIHandler(APIHandler):
 
-    @authenticated
-    @as_json
-    def get(self, username):
-        return None
-
-    @authenticated
-    @as_json
-    def post(self):
-        return None
-
-    @authenticated
-    @as_json
-    def delete(self):
-        return None
-
-
-class AuthedBlockedAPIHandler(APIHandler):
-
-    @authenticated
     @as_json
     def get(self):
         return None
 
-    @authenticated
+
+class FollowOneAPIHandler(APIHandler):
+
+    @as_json
+    def post(self, username):
+        return None
+
+
+class UnfollowOneAPIHandler(APIHandler):
+
+    @as_json
+    def delete(self, username):
+        return None
+
+
+class BlockedAPIHandler(APIHandler):
+
+    @as_json
+    def get(self):
+        return None
+
+
+class BlockOneAPIHandler(APIHandler):
+
     @as_json
     def post(self):
         return None
 
-    @authenticated
+
+class UnblockOneAPIHandler(APIHandler):
+
     @as_json
     def delete(self):
         return None
@@ -65,18 +69,22 @@ class AuthedBlockedAPIHandler(APIHandler):
 urls = [
     # `GET /api/users/:username`, get information of username.
     (r'/api/users/(\w+)', UsersAPIHandler),
+
     # For the current user:
+
     #  `GET /api/user`, get user information.
     #  `PATCH /api/user`, update user information.
-    (r'/api/user', AuthedUserAPIHandler),
-    # For the current user:
+    (r'/api/user', UserAPIHandler),
     #  `GET /api/user/following`, get all following users.
-    #  `POST /api/user/following`, follow a new user.
-    #  `DELETE /api/user/following`, unfollow a user.
-    (r'/api/user/following', AuthedFollowingAPIHandler),
-    # For the current user:
+    (r'/api/user/following', FollowingAPIHandler),
+    #  `POST /api/user/follow/:username`, follow a new user.
+    (r'/api/user/follow/(\w+)', FollowOneAPIHandler),
+    #  `DELETE /api/user/unfollow/:username`, unfollow a user.
+    (r'/api/user/unfollow/(\w+)', UnfollowOneAPIHandler),
     #  `GET /api/user/blocked`, get all bloacked users.
-    #  `POST /api/user/blocked`, blocked a new user.
-    #  `DELETE /api/user/blocked`, unblock a user.
-    (r'/api/user/blocked', AuthedBlockedAPIHandler),
+    (r'/api/user/blocked', BlockedAPIHandler),
+    #  `POST /api/user/block/:username`, blocked a new user.
+    (r'/api/user/block/(\w+)', BlockOneAPIHandler),
+    #  `DELETE /api/user/unblock/:username`, unblock a user.
+    (r'/api/user/unblock/(\w+)',UnblockOneAPIHandler),
 ]
