976dd6c61b5d3e80b6de4ae1c00560363bd6d8a4
Damnever <dxc.wolf@gmail.com>
2015-12-27 23:52:46 +0800

    pip supported

---
+++ setup.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+
+
+setup(
+    name='2L',
+    version='0.0.1',
+    description='A powerful forum',
+    long_description='to be continued',
+    url='https://github.com/Damnever/2L',
+    author='Damnever',
+    author_emial='dxc.wolf@gmail.com',
+    license='The BSD 3-Clause License',
+    keywords='forum, Python',
+    classifiers=[
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+    ],
+)
