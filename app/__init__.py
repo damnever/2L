fb1522c8b082af6c7a308efb8b4ed39144b151a4
Damnever <dxc.wolf@gmail.com>
2015-12-28 21:33:38 +0800

    work with pip

--- app/__init__.py
+++ app/__init__.py
@@ -1,3 +1,11 @@
 # -*- coding: utf-8 -*-
 
 from __future__ import print_function, division, absolute_import
+
+import sys
+
+
+# Speed up...
+if sys.version_info[0] < 3:
+    import __builtin__
+    __builtin__.range = __builtin__.xrange
--- setup.py
+++ setup.py
@@ -6,16 +6,21 @@ from setuptools import setup
 setup(
     name='2L',
     version='0.0.1',
-    description='A powerful forum',
-    long_description='to be continued',
+    description='2L, There is a surprise!',
+    long_description='2L, SB! BIG S! BIG B! BIG SB!!!',
     url='https://github.com/Damnever/2L',
     author='Damnever',
     author_emial='dxc.wolf@gmail.com',
     license='The BSD 3-Clause License',
-    keywords='forum, Python',
+    keywords='bbs, forum, 2L',
     classifiers=[
         'License :: OSI Approved :: BSD License',
         'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 3',
     ],
+    entry_points={
+        'console_scripts': [
+            '2L=app.app:run',
+        ]
+    },
 )
