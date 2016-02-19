2389a5d1ea4b79cc29346d64caef8f74f3ae5305
Damnever <dxc.wolf@gmail.com>
2016-02-19 10:57:33 +0800

    c extension

--- app/libs/sorts.pyx
+++ app/libs/sorts.pyx
@@ -11,24 +11,29 @@ from datetime import datetime
 from tzlocal import get_localzone
 
 
+cdef extern from "math.h":
+    double log10(double)
+    double sqrt(double)
+
+
 #----------------------------------------------------------------------
 # Post ranking.
 #----------------------------------------------------------------------
 epoch = datetime(1970, 1, 1, tzinfo=get_localzone())
 
 
-cdef double epoch_seconds(date):
+cpdef double epoch_seconds(date):
     td = date - epoch
     return td.days * 86400 + td.seconds + (float(td.microseconds) / 1000000)
 
 
-cdef int score(ups, downs):
+cpdef long score(ups, downs):
     return ups - downs
 
 
-cdef hot(ups, downs, date):
+cpdef double hot(ups, downs, date):
     s = score(ups, downs)
-    order = math.log10(max(abs(s), 1))
+    order = log10(max(abs(s), 1))
 
     if s > 0:
         sign = 1
@@ -43,7 +48,7 @@ cdef hot(ups, downs, date):
 #----------------------------------------------------------------------
 # Comment ranking.
 #----------------------------------------------------------------------
-cdef _confidence(ups, downs):
+cpdef double _confidence(ups, downs):
     n = ups + downs
 
     if n == 0:
@@ -52,20 +57,21 @@ cdef _confidence(ups, downs):
     z = 1.281551565545  # 80% confidence
     p = ups / n
     left = p + 1/(2*n)*z*z
-    right = z*math.sqrt(p*(1-p)/n + z*z/(4*n*n))
+    right = z*sqrt(p*(1-p)/n + z*z/(4*n*n))
     under = 1+1/n*z*z
 
     return (left - right) / under
 
 
-up_range, down_range = 400, 100
-_confidences = list()
+cdef int up_range = 400
+cdef int down_range = 100
+cdef list _confidences = []
 for ups in range(up_range):
     for downs in range(down_range):
         _confidences.append(_confidence(ups, downs))
 
 
-cdef confidence(ups, downs):
+def confidence(ups, downs):
     if ups + downs == 0:
         return 0
     elif ups < up_range and downs < down_range:
